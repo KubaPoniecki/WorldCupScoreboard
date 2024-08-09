9bb6cae5d00c2239accca7e4b6ad3fd29d8ac46a
Jakub P <ponieckijakub@gmail.com>
2024-08-09 10:55:16 +0200

    Configured the environment.

---
+++ src/main/java/org/scoreboard/App.java
@@ -0,0 +1,13 @@
+package org.scoreboard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
